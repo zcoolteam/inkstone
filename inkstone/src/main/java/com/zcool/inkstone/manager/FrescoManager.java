fe605df8831aa0d5d601b7659e600f0b955b4b6d
idonans <idonans@126.com>
2018-12-05 12:56:56 +0800

    add FrescoManager#fetchImageThumb

--- inkstone/src/main/java/com/zcool/inkstone/manager/FrescoManager.java
+++ inkstone/src/main/java/com/zcool/inkstone/manager/FrescoManager.java
@@ -1,5 +1,6 @@
 package com.zcool.inkstone.manager;
 
+import android.graphics.Bitmap;
 import android.text.TextUtils;
 
 import com.facebook.cache.disk.DiskCacheConfig;
@@ -15,6 +16,8 @@ import com.facebook.imageformat.ImageFormat;
 import com.facebook.imageformat.ImageFormatChecker;
 import com.facebook.imagepipeline.backends.okhttp3.OkHttpNetworkFetcher;
 import com.facebook.imagepipeline.core.ImagePipelineConfig;
+import com.facebook.imagepipeline.datasource.BaseBitmapDataSubscriber;
+import com.facebook.imagepipeline.image.CloseableImage;
 import com.facebook.imagepipeline.request.ImageRequest;
 import com.zcool.inkstone.Debug;
 import com.zcool.inkstone.lang.Singleton;
@@ -24,6 +27,7 @@ import com.zcool.inkstone.util.FileUtil;
 import com.zcool.inkstone.util.IOUtil;
 
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.InputStream;
 import java.util.concurrent.Executor;
 
@@ -131,6 +135,81 @@ public class FrescoManager {
 
                             @Override
                             protected void onFailureImpl(DataSource<CloseableReference<PooledByteBuffer>> dataSource) {
+                                Throwable e;
+                                if (dataSource == null) {
+                                    e = new IllegalStateException("onFailureImpl throw: dataSource is null");
+                                } else {
+                                    e = new IllegalStateException("onFailureImpl throw", dataSource.getFailureCause());
+                                }
+                                observer.onError(e);
+                            }
+                        }, new Executor() {
+                            @Override
+                            public void execute(Runnable command) {
+                                Threads.postBackground(command);
+                            }
+                        });
+            }
+        };
+    }
+
+    public SingleSource<File> fetchImageThumb(final ImageRequest imageRequest) {
+        return fetchImageThumb(imageRequest, null);
+    }
+
+    public SingleSource<File> fetchImageThumb(final ImageRequest imageRequest, @Nullable final Bitmap.CompressFormat format) {
+        return new SingleSource<File>() {
+            @Override
+            public void subscribe(final SingleObserver<? super File> observer) {
+                Fresco.getImagePipeline().fetchDecodedImage(imageRequest, null)
+                        .subscribe(new BaseBitmapDataSubscriber() {
+                            @Override
+                            protected void onNewResultImpl(@javax.annotation.Nullable Bitmap bitmap) {
+                                File errorTmpFile = null;
+                                FileOutputStream fos = null;
+                                try {
+                                    if (bitmap == null) {
+                                        throw new IllegalArgumentException("bitmap is null");
+                                    }
+
+                                    final String suffix;
+                                    final Bitmap.CompressFormat safeFormat;
+                                    if (format == Bitmap.CompressFormat.WEBP) {
+                                        suffix = "webp";
+                                        safeFormat = format;
+                                    } else if (format == Bitmap.CompressFormat.PNG) {
+                                        suffix = "png";
+                                        safeFormat = format;
+                                    } else {
+                                        suffix = "jpeg";
+                                        safeFormat = Bitmap.CompressFormat.JPEG;
+                                    }
+
+                                    File targetFile = TmpFileManager.getInstance().createNewTmpFileQuietly(null, "." + suffix);
+                                    if (targetFile == null) {
+                                        throw new IllegalAccessException("TmpFileManager create target file return null");
+                                    }
+
+                                    errorTmpFile = targetFile;
+                                    fos = new FileOutputStream(targetFile);
+
+                                    if (!bitmap.compress(safeFormat, 85, fos)) {
+                                        throw new IllegalAccessException("fail to compress bitmap to " + suffix + " file");
+                                    }
+                                    errorTmpFile = null;
+
+                                    observer.onSuccess(targetFile);
+                                } catch (Throwable e) {
+                                    e.printStackTrace();
+                                    observer.onError(e);
+                                } finally {
+                                    IOUtil.closeQuietly(fos);
+                                    FileUtil.deleteFileQuietly(errorTmpFile);
+                                }
+                            }
+
+                            @Override
+                            protected void onFailureImpl(DataSource<CloseableReference<CloseableImage>> dataSource) {
                                 Throwable e;
                                 if (dataSource == null) {
                                     e = new IllegalStateException("onFailureImpl throw: dataSource is null");
