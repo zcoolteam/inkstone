a765c20ef7369c740761b53b6e94c95c09d15104
idonans <idonans@126.com>
2019-01-14 18:02:57 +0800

    update inkstone gradle plugin, gen com.zcoo.inkstone.InkstoneAppConfigImpl with config R classes

--- inkstone-gradle-plugin/src/main/kotlin/com/zcool/inkstone/gradle/plugin/InkstoneConfigClassBuilder.kt
+++ inkstone-gradle-plugin/src/main/kotlin/com/zcool/inkstone/gradle/plugin/InkstoneConfigClassBuilder.kt
@@ -1,6 +1,10 @@
 package com.zcool.inkstone.gradle.plugin
 
+import com.squareup.javapoet.*
+import com.zcool.inkstone.annotation.Config
+import com.zcool.inkstone.annotation.ModuleConfig
 import java.io.File
+import javax.lang.model.element.Modifier
 
 class InkstoneConfigClassBuilder {
 
@@ -11,9 +15,26 @@ class InkstoneConfigClassBuilder {
     }
 
     fun create(outputDir: File) {
-        for (rClassPackageName in rClassPackageNames) {
-            println("rClassPackageName -> $rClassPackageName")
+        val codeBlockBuilder = CodeBlock.builder()
+        codeBlockBuilder.addStatement("\$T result = new \$T()", Config::class.java, Config::class.java)
+        rClassPackageNames.forEach {
+            val moduleConfigClassName = ClassName.bestGuess(it.substring(0, it.length - 1) + "InkstoneModuleConfigImpl")
+            codeBlockBuilder.addStatement("result.add(new \$T())", moduleConfigClassName)
         }
+        codeBlockBuilder.addStatement("return result")
+
+        val targetClassBuilder = TypeSpec.classBuilder("InkstoneAppConfigImpl")
+                .addModifiers(Modifier.PUBLIC, Modifier.FINAL)
+                .addSuperinterface(ModuleConfig::class.java)
+                .addMethod(MethodSpec.methodBuilder("getConfig")
+                        .addModifiers(Modifier.PUBLIC)
+                        .returns(Config::class.java)
+                        .addAnnotation(Override::class.java)
+                        .addCode(codeBlockBuilder.build())
+                        .build())
+        JavaFile.builder("com.zcool.inkstone", targetClassBuilder.build())
+                .build()
+                .writeTo(outputDir)
     }
 
 }
