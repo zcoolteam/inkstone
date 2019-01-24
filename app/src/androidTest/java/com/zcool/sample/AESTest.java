3824f6f10ca4308e7b7216b37a86e81fb7fcd38f
idonans <idonans@126.com>
2019-01-24 18:40:34 +0800

    add AES test code

--- app/src/androidTest/java/com/zcool/sample/AESTest.java
+++ app/src/androidTest/java/com/zcool/sample/AESTest.java
@@ -24,18 +24,25 @@ public class AESTest {
         Inkstone.init(context);
 
         List<String> inputs = AssetUtil.readAllLines("AESTestData", null, null);
+        List<String> inputs1 = AssetUtil.readAllLines("AESTestData_1", null, null);
 
-        inputs.forEach(input -> {
-            try {
-                String encode = AES.getDefault().encode(input);
-                String decode = AES.getDefault().decode(encode);
-                System.out.println(input + " >>>>> " + encode + " >>>>> " + decode);
-                Assert.assertEquals(input, decode);
-            } catch (Throwable e) {
-                e.printStackTrace();
-                throw new RuntimeException(e);
-            }
-        });
+        Assert.assertEquals(inputs.size(), inputs1.size());
+
+        int size = inputs.size();
+        for (int i = 0; i < size; i++) {
+            String input = inputs.get(i);
+            String input1 = inputs1.get(i);
+
+            String encode = AES.getDefault().encode(input);
+            String decode = AES.getDefault().decode(encode);
+
+            System.out.println(input + " >>>>> " + encode + " >>>>> " + decode);
+            Assert.assertEquals(input, decode);
+
+            String decode1 = AES.getDefault().decode(input1);
+            System.out.println(" >>>>> " + input1 + " >>>>> " + decode1);
+            Assert.assertEquals(input, decode1);
+        }
     }
 
 }
