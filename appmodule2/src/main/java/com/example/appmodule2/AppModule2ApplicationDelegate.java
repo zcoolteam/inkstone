3d2169526c0dc4079c861d2a1085f7376e0fab5c
idonans <idonans@126.com>
2019-02-27 15:34:38 +0800

    remove appmodule2 AppModule2ApplicationDelegate config

--- appmodule2/src/main/java/com/example/appmodule2/AppModule2ApplicationDelegate.java
+++ appmodule2/src/main/java/com/example/appmodule2/AppModule2ApplicationDelegate.java
@@ -3,13 +3,11 @@ package com.example.appmodule2;
 import android.content.Context;
 
 import com.zcool.inkstone.ModuleApplicationDelegate;
-import com.zcool.inkstone.annotation.ApplicationDelegate;
 
 import androidx.annotation.Keep;
 import timber.log.Timber;
 
 @Keep
-@ApplicationDelegate(priority = 1)
 public class AppModule2ApplicationDelegate implements ModuleApplicationDelegate {
 
     @Override
