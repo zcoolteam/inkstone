c51d6a55e7e5f898138fb5e793a0c17126f16d15
idonans <idonans@126.com>
2019-03-01 14:59:07 +0800

    add webview setDataDirectorySuffix

--- inkstone/src/main/java/com/zcool/inkstone/InkstoneApplicationDelegate.java
+++ inkstone/src/main/java/com/zcool/inkstone/InkstoneApplicationDelegate.java
@@ -1,9 +1,12 @@
 package com.zcool.inkstone;
 
 import android.content.Context;
+import android.os.Build;
+import android.webkit.WebView;
 
 import com.zcool.inkstone.annotation.ApplicationDelegate;
 import com.zcool.inkstone.manager.FrescoManager;
+import com.zcool.inkstone.manager.ProcessManager;
 import com.zcool.inkstone.service.InkstoneService;
 import com.zcool.inkstone.util.ContextUtil;
 
@@ -15,6 +18,10 @@ public class InkstoneApplicationDelegate implements ModuleApplicationDelegate {
     @Override
     public void onCreate(Context context) {
         Timber.v("[priority default] onCreate");
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            WebView.setDataDirectorySuffix(String.valueOf(ProcessManager.getInstance().getProcessTag()));
+        }
 
         FrescoManager.getInstance();
     }
